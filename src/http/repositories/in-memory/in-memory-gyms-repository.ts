0d45fdbf4b8f1a0b75abe6a9bad53b777c0ac872
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-15 21:37:16 -0300

    feat: Implement fetch for nearby gyms use case

--- src/http/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/http/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from 'crypto'
-import { GymsRepository } from '../gyms-repository'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { Gym, Prisma } from '@prisma/client'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distante-between-coordinates'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public gyms: Gym[] = []
@@ -35,5 +36,19 @@ export class InMemoryGymsRepository implements GymsRepository {
     return this.gyms
       .filter((item) => item.title.includes(query))
       .slice((page - 1) * 20, page * 20)
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.gyms.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
   }
 }
