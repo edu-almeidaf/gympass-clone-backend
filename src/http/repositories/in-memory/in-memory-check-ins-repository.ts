5dfd3413909874efc8c3bf005663d9328788b0aa
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-15 22:18:44 -0300

    feat: Implement check-in validation use case

--- src/http/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/http/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -49,4 +49,26 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
   async countByUserId(userId: string) {
     return this.checkIns.filter((checkIn) => checkIn.user_id === userId).length
   }
+
+  async findById(id: string) {
+    const checkIn = this.checkIns.find((item) => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.checkIns.findIndex(
+      (item) => item.id === checkIn.id,
+    )
+
+    if (checkInIndex >= 0) {
+      this.checkIns[checkInIndex] = checkIn
+    }
+
+    return checkIn
+  }
 }
